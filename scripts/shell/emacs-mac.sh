986aa26baf87719a95f43718f335655fd1a30064
sfulle176 <Seth_Fuller@comcast.com>
2021-06-18 12:27:28 -0400

    Changed path to Emacs app

--- scripts/shell/emacs-mac.sh
+++ scripts/shell/emacs-mac.sh
@@ -2,9 +2,22 @@
 PROFILE=$1
 if [ -z "$PROFILE" ]
 then
-    PROFILE="legacy"
+    PROFILE="default"
 fi
 
+VERSION=""
+VERS_US=""
+if [ $# -gt 1 ]
+then
+    VERSION="$2"
+    VERS_US="_$VERSION"
+fi
+
+echo "\$#=$#"
+
 set -x
-/usr/local/opt/emacs-mac/Emacs.app/Contents/MacOS/Emacs --with-profile $PROFILE &
+# /usr/local/opt/emacs-plus${VERS_US}/Emacs${VERSION}.app/Contents/MacOS/Emacs --with-profile $PROFILE $ICON &
+
+/Applications/Emacs${VERSION}.app/Contents/MacOS/Emacs --with-profile $PROFILE &
+
 set +x
