81b190cd678ac3c82ae85c383f7833549d76cac9
sfulle176 <Seth_Fuller@comcast.com>
2021-06-24 14:47:12 -0400

    Building more of the script

--- scripts/python/mvn_src_jd.py
+++ scripts/python/mvn_src_jd.py
@@ -6,6 +6,7 @@ class MvnSrcJavadoc:
     def __init__(self):
         self.parse_args()
         self.validate_args()
+        self.init_cmds()
 
     def parse_args(self):
         # Process command line arguments
@@ -33,11 +34,42 @@ class MvnSrcJavadoc:
 
         self.args = parser.parse_args()
 
+    def init_cmds(self):
+        dep_sources = 'dependency:sources'
+        dep_javadoc = 'dependency:resolve -Dclassifier=javadoc'
+        self.cmds = {
+            'all': f'{dep_sources} {dep_javadoc}',
+            'source': dep_sources,
+            'javadoc': dep_javadoc
+        }
+
+        self.art_grp = {
+            'artifact': '-DincludeArtifactIds=',
+            'group': '-DincludeGroupIds=',
+        }
+
     def validate_args(self):
         print(self.args)
 
+    def run_mvn(self):
+        mvn_cmd = ''
+        mvn_args = ''
+
+        if self.args.all:
+            mvn_cmd =self.cmds['all']
+        if self.args.source:
+            mvn_cmd =self.cmds['source']
+        if self.args.javadoc:
+            mvn_cmd =self.cmds['javadoc']
+
+        if self.args.artifact_id:
+            mvn_args = f'{self.art_grp["artifact"]}{self.args.artifact_id} {self.art_grp["group"]}{self.args.group_id}'
+
+        mvn_cmd_line = f'mvn {mvn_cmd} {mvn_args}'
+        print(mvn_cmd_line)
+
 def main():
     mvn_src_javadoc = MvnSrcJavadoc()
+    mvn_src_javadoc.run_mvn()
 
-    
 main()
