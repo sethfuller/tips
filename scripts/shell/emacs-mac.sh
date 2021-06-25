76f58c416e5b6e57cec48beb84f84af162a8ff76
sfulle176 <Seth_Fuller@comcast.com>
2021-06-25 17:03:35 -0400

    Added options

--- scripts/shell/emacs-mac.sh
+++ scripts/shell/emacs-mac.sh
@@ -6,16 +6,57 @@
 #     shift
 # fi
 
-PROFILE="default"
+message() {
+    echo $*
+    echo ""
+}
 
-echo "\$#=$#"
+PARSED_ARGS=$(getopt p:d $*)
+VALID_ARGUMENTS=$?
 
-set -x
-# /usr/local/opt/emacs-plus${VERS_US}/Emacs${VERSION}.app/Contents/MacOS/Emacs --with-profile $PROFILE $ICON &
+if [ "$VALID_ARGUMENTS" != "0" ]; then
+  usage
+fi
+
+set -- $PARSED_ARGS
+
+DEFAULT_PROFILE=""
+for opt
+do
+    case "$opt"
+    in
+        -p)
+            PROFILE="$2"; shift;
+            shift;;
+        -d)
+            DEFAULT_PROFILE="1"
+            shift;;
+        --)
+            shift; break;;
+    esac
+done
+
+if [ ! -z "$DEFAULT_PROFILE" ]
+then
+    PROFILE="default"
+fi
+
+EMACS_ARGS=""
+
+if [ ! -z "$PROFILE" ]
+then
+    EMACS_ARGS="--with-profile $PROFILE"
+fi
 
 for VERSION in $*
 do
-    /Applications/Emacs${VERSION}.app/Contents/MacOS/Emacs --with-profile $PROFILE &
+    EMACS_APP="/Applications/Emacs${VERSION}.app/Contents/MacOS/Emacs"
+    if [ -f "$EMACS_APP" ]
+    then
+        echo "START: $EMACS_APP $EMACS_ARGS &"
+        $EMACS_APP $EMACS_ARGS &
+    else
+        echo "$EMACS_APP - DOES NOT EXIST"
+    fi
 done
 
-set +x
