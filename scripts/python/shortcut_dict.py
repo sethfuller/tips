c7c3f7164ffe1009d68cde3e575633099eade067
sfulle176 <Seth_Fuller@comcast.com>
2021-06-29 11:33:54 -0400

    Added script to display shortcuts

---
+++ scripts/python/shortcut_dict.py
@@ -0,0 +1,117 @@
+
+shortcuts = {
+    'mac': [
+        {
+            'keys': '<Control>-<Command>-F',
+            'description': 'Toggle Full Screen'
+        },
+        {
+            'keys': '-<Command>-W',
+            'description': 'Close Current Window of Application'
+        },
+        {
+            'keys': '<Control>-<Command>-F',
+            'description': 'Toggle Full Screen'
+        },
+        {
+            'keys': '<Control>-<Down Arrow>',
+            'description': 'Show only Current Window of Current Application'
+        },
+        {
+            'keys': '<Control>-<Up Arrow>',
+            'description': 'Show all Windows Application'
+        },
+    ],
+    'iterm': [
+        {
+            'keys': '<Command>-<Left Arrow>',
+            'description': 'Next Tab',
+        },
+        {
+            'keys': '<Command>-<Right Arrow>',
+            'description': 'Previous Tab',
+        },
+        {
+            'keys': '<Command>-<number>',
+            'description': 'Go to Tab Number in Current Window',
+        },
+        {
+            'keys': '<Command>-<Option/Alt>-<number>',
+            'description': 'Go to Window Number (by opened order)',
+        },
+        {
+            'keys': '<Command>-T',
+            'description': 'New Tab',
+        },
+        {
+            'keys': '<Command>-N',
+            'description': 'New Window',
+        },
+    ],
+    'chrome': [
+        {
+            'keys': '<Command>-<Option/Alt>-<Left Arrow>',
+            'description': 'Go to Next Tab',
+        },
+        {
+            'keys': '<Command>-<Option/Alt>-<Right Arrow>',
+            'description': 'Go to Previous Tab',
+        },
+        {
+            'keys': '<Command>-<number>',
+            'description': 'Go to Tab Number (1-8) in Current Window',
+        },
+        {
+            'keys': '<Command>-9',
+            'description': 'Go to Last Tab in Current Window',
+        },
+        {
+            'keys': '<Command>-<Left Mouse Click>',
+            'description': 'Open Link New Tab',
+        },
+        {
+            'keys': '<Shift>-<Left Mouse Click>',
+            'description': 'Open Link New Window',
+        },
+        {
+            'keys': '<Command>-` (backtick)',
+            'description': 'Cycle Between Open Windows',
+        },
+    ],
+    'intellij': [
+        {
+            'keys': '<Shift>-<Command>-F',
+            'description': 'Search Files Below Directory Specified',
+        },
+        {
+            'keys': '<Command>-F',
+            'description': 'Search Current File',
+        },
+        {
+            'keys': '<Command>-O',
+            'description': 'Find a Class by Name',
+        },
+        {
+            'keys': '<Ctrl>-D',
+            'description': 'Run Process in Debug Mode',
+        },
+        {
+            'keys': '<Ctrl>-R',
+            'description': 'Run Process',
+        },
+        {
+            'keys': '<Command>-<F2>',
+            'description': 'Stop Running Process',
+        },
+    ],
+    'dummy': [
+        {
+            'keys': '<Shift>-<Option/Alt>-<Command>',
+            'description': '',
+        },
+        {
+            'keys': '<Shift>-<Option/Alt>-<Command>',
+            'description': '',
+        },
+    ],
+}
---
+++ scripts/python/shortcuts.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+
+import argparse
+import shortcut_dict
+
+class Shortcuts:
+    """
+    """
+
+    def __init__(self):
+        self.init_args()
+        self.validate_args()
+
+    def init_args(self):
+        # Process command line arguments
+        parser = argparse.ArgumentParser(description='Get mvn sources/javadocs')
+        parser.add_argument('--log_level', '-l',
+                            help='Set the log level (Default INFO)',
+                           default='INFO')
+
+        parser.add_argument('--mac', '-m',
+                            help='show Mac shortcuts',
+                            action='store_true')
+
+        parser.add_argument('--chrome', '-c',
+                            help='show Chrome shortcuts',
+                            action='store_true')
+
+        parser.add_argument('--intellij', '-i',
+                            help='show Intellij shortcuts',
+                            action='store_true')
+
+        parser.add_argument('--iterm', '-t',
+                            help='show iTerm2 shortcuts',
+                            action='store_true')
+
+
+        self.args = parser.parse_args()
+        self.parser = parser
+
+    def validate_args(self):
+        print(self.args)
+        has_errors = False
+
+        if  not self.args.chrome and not self.args.mac and not self.args.intellij and not self.args.iterm:
+            print('Must pick one of  --chrome or --mac or --intellij or --iterm')
+            has_errors = True
+
+        if has_errors:
+            self.parser.print_help()
+            print('Exiting')
+            sys.exit(1)
+
+    def display_shortcuts(self):
+        if self.args.mac:
+            self.print_shortcuts('Mac', shortcut_dict.shortcuts.get('mac'))
+
+        if self.args.chrome:
+            self.print_shortcuts('Chrome', shortcut_dict.shortcuts.get('chrome'))
+
+        if self.args.intellij:
+            self.print_shortcuts('Intellij', shortcut_dict.shortcuts.get('intellij'))
+
+        if self.args.iterm:
+            self.print_shortcuts('iTerm2', shortcut_dict.shortcuts.get('iterm'))
+
+    def print_shortcuts(self, title, shortcut_list):
+        print(title + ':')
+        if shortcut_list:
+            for shortcut in shortcut_list:
+                print(f'\tKeys: {shortcut.get("keys")} {shortcut.get("description")}')
+        else:
+            print(f'Shortcuts not found for {title}')
+
+        print('')
+
+def main():
+    shortcuts = Shortcuts()
+    shortcuts.display_shortcuts()
+
+main()
