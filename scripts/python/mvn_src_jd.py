9a8690d5823603aaeb0eaec80664493c2604e0ed
sfulle176 <Seth_Fuller@comcast.com>
2021-06-29 10:30:07 -0400

    Added code to run mvn command

--- scripts/python/mvn_src_jd.py
+++ scripts/python/mvn_src_jd.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python3
 
 import argparse
+import sys
+import general_utils as gen_utils
 
 class MvnSrcJavadoc:
+    """
+    Get the source and/or javadoc for all dependencies defined in a pom.xml
+    or for a specific dependency.
+    """
+
     def __init__(self):
         self.parse_args()
         self.validate_args()
@@ -33,6 +40,7 @@ class MvnSrcJavadoc:
                             action='store_true')
 
         self.args = parser.parse_args()
+        self.parser = parser
 
     def init_cmds(self):
         dep_sources = 'dependency:sources'
@@ -43,13 +51,23 @@ class MvnSrcJavadoc:
             'javadoc': dep_javadoc
         }
 
-        self.art_grp = {
+        self.art_grp_args = {
             'artifact': '-DincludeArtifactIds=',
             'group': '-DincludeGroupIds=',
         }
 
     def validate_args(self):
         print(self.args)
+        has_errors = False
+
+        if not self.args.all and not self.args.source and not self.args.javadoc:
+            print('Must pick one of --all --source or --javadoc')
+            has_errors = True
+
+        if has_errors:
+            self.parser.print_help()
+            print('Exiting')
+            sys.exit(1)
 
     def run_mvn(self):
         mvn_cmd = ''
@@ -63,10 +81,25 @@ class MvnSrcJavadoc:
             mvn_cmd =self.cmds['javadoc']
 
         if self.args.artifact_id:
-            mvn_args = f'{self.art_grp["artifact"]}{self.args.artifact_id} {self.art_grp["group"]}{self.args.group_id}'
+            mvn_args = f'{self.art_grp_args["artifact"]}{self.args.artifact_id} {self.art_grp_args["group"]}{self.args.group_id}'
 
         mvn_cmd_line = f'mvn {mvn_cmd} {mvn_args}'
         print(mvn_cmd_line)
+        process = gen_utils.run_process(mvn_cmd_line)
+
+        process_output = gen_utils.read_process_output(process, {'stdout': True, 'stderr': True})
+
+        output_lines = process_output.get('stdout')
+        print('STDOUT')
+        for line in output_lines:
+            print(line)
+
+        output_lines = process_output.get('stderr')
+        print('STDERR')
+        for line in output_lines:
+            print(line)
+
+        gen_utils.process_ret_code(process, mvn_cmd_line)
 
 def main():
     mvn_src_javadoc = MvnSrcJavadoc()
