d394a57cb30d49ededc5d1e4e9aa0a0781a8d8d1
sfulle176 <Seth_Fuller@comcast.com>
2021-07-07 16:53:11 -0400

    Added profiles

--- config/Emacs/MultipleConfigs/.emacs-profiles.el
+++ config/Emacs/MultipleConfigs/.emacs-profiles.el
@@ -1,5 +1,9 @@
-;; https://github.com/plexus/chemacs2.git
 (
  ("default" . ((user-emacs-directory . "~/.emacs.legacy")))
- ("doom"   . ((user-emacs-directory . "~/doom-emacs")))
+ ("emacs-1" . ((user-emacs-directory . "~/.emacs.1")))
+ ("emacs-2" . ((user-emacs-directory . "~/.emacs.2")))
+ ("emacs-3" . ((user-emacs-directory . "~/.emacs.3")))
+ ("emacs-4" . ((user-emacs-directory . "~/.emacs.4")))
+ ("emacs-5" . ((user-emacs-directory . "~/.emacs.5")))
+ ("emacs-6" . ((user-emacs-directory . "~/.emacs.6")))
  )
