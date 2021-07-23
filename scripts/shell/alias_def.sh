3341113946b6653f1b8fe43fae69da0b1c841ab5
sfulle176 <Seth_Fuller@comcast.com>
2021-07-23 10:01:39 -0400

    Added link to answer for finding lines executed on startup

--- scripts/shell/alias_def.sh
+++ scripts/shell/alias_def.sh
@@ -1,6 +1,7 @@
 
 ARGS=$*
 
+# Found in https://stackoverflow.com/questions/2614403/how-to-find-out-where-alias-in-the-bash-sense-is-defined-when-running-terminal/43245187#26742455
 # -i - interactive
 # -x - xtrace (echo commands)
 # -c - Used on the command line to specify a single command
