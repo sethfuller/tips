e5fdd4e5c09418e0652c9f8432882821a3af12bc
Seth Fuller <fuller.sethm@gmail.com>
2024-10-25 15:49:14 -0400

    Fix so it only shows alias definitions

--- scripts/shell/other/alias_def.sh
+++ scripts/shell/other/alias_def.sh
@@ -6,4 +6,4 @@ ARGS=$*
 # -x - xtrace (echo commands)
 # -c - Used on the command line to specify a single command
 # : - Equivalent of true
-zsh -ixc : 2>&1 |grep "$ARGS"
+zsh -ixc : 2>&1 |grep "$ARGS " | grep 'alias ' | grep -Ev 'compdump|compaudit'
